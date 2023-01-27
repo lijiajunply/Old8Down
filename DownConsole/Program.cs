c66e1d808d86ddc6ac5c9992f6ad6f82d3e7d1cf
李嘉俊好牛的 <2383690354@qq.com>
2023-01-27 19:15:23 +0800

    asdf

---
+++ DownConsole/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.IO;
+using System.Text.RegularExpressions;
+using DownRegex;
+
+
+var a = APIs.GetTexts("/home/luckyfish/RiderProjects/Old8Down/DownRegex/CSS/Ex.od");
+foreach (var variable in a)
+    Console.WriteLine(variable);
+var html = APIs.DownToHTML("/home/luckyfish/RiderProjects/Old8Down/DownRegex/CSS/Ex.od");
+Console.WriteLine(html);
+File.WriteAllText("/home/luckyfish/RiderProjects/Old8Down/DownRegex/CSS/Ex.html",html);
